a9dbd1a6d8b549239367a5f83d5375d71af304b8
Ernest Bruno <erndobruno@gmail.com>
2022-04-28 10:53:06 -0700

    Stub up components.

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Navbar = () => {
+    
+  return (
+    <div className="navbar-container">
+        <ul className="navbar-list">
+            <li className={`navbar-item `}>NavItem 1</li>
+            <li className={`navbar-item `}>NavItem 2</li>
+            <li className={`navbar-item `}>NavItem 3</li>
+        </ul>
+    </div>
+  )
+}
+
+export default Navbar;
