9693605226905f30aa9fcc084f6f10371070ceae
Ernest Bruno <erndobruno@gmail.com>
2022-04-30 22:34:47 -0700

    Change how navbar is rendered. Also add scroll handling, apply styles.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,34 @@
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect } from "react";
 import { Navbar, About, Projects, Skills, Contact, Footer } from "./components";
 import "./App.css";
 
 function App() {
     const scrollRef = useRef({});
+    const [currSection, setCurrentSection] = useState('about');
+
+    const handleScroll = (e) => {
+      const refCurr = scrollRef.current;
+      if (window.scrollY <= refCurr.about.offsetTop + 20) {
+        setCurrentSection('about');
+      } else if (window.scrollY <= refCurr.projects.offsetTop + 20 && window.scrollY > refCurr.projects.offsetTop - 20) {
+        setCurrentSection('projects');
+      } else if (window.scrollY <= refCurr.skills.offsetTop + 20 && window.scrollY > refCurr.skills.offsetTop - 20) {
+        setCurrentSection('skills');
+      } else if (window.scrollY <= refCurr.contact.offsetTop + 20 && window.scrollY > refCurr.contact.offsetTop - 20) {
+        setCurrentSection('contact');
+      }
+    }
+    
+    useEffect(() => {
+      window.addEventListener('scroll', handleScroll, {passive: true});
+      return () => {
+        window.removeEventListener('scroll', handleScroll)
+      }
+    }, [scrollRef.current]);
 
     return (
         <div className="App">
-            <Navbar scrollRef={scrollRef}/>
+            <Navbar currSection={currSection} scrollRef={scrollRef}/>
             <div className="app-container">
                 <About scrollRef={scrollRef} />
                 <Projects scrollRef={scrollRef} />
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,30 @@
-import React from 'react';
-import '../assets/styles/Navbar.css';
+import React from "react";
+import "../assets/styles/Navbar.css";
 
-const Navbar = ({scrollRef}) => {    
+const Navbar = ({ scrollRef, currSection }) => {
+    const scrollIntoView = (type) => {
+        scrollRef.current[type].scrollIntoView({ behavior: "smooth" });
+    };
 
-  const scrollIntoView = (type) => {
-    scrollRef.current[type].scrollIntoView({behavior: 'smooth'})
-  }
-  return (
-    <div className="navbar-container">
-        <ul className="navbar-list">
-            <li className={`navbar-item `} onClick={() => scrollIntoView('about')}>About</li>
-            <li className={`navbar-item `} onClick={() => scrollIntoView('projects')}>Projects</li>
-            <li className={`navbar-item `} onClick={() => scrollIntoView('skills')}>Skills</li>
-            <li className={`navbar-item `} onClick={() => scrollIntoView('contact')}>Contact</li>
-        </ul>
-    </div>
-  )
-}
+    const listItems = ["about", "projects", "skills", "contact"];
+
+    return (
+        <div className="navbar-container">
+            <ul className="navbar-list">
+                {listItems.map((l, i) => (
+                    <li
+                        key={i}
+                        className={`navbar-item 
+                        ${ currSection === l ? 'selected': ''}
+                        `}
+                        onClick={() => scrollIntoView(l)}
+                    >
+                        {l.toUpperCase()}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+};
 
 export default Navbar;
